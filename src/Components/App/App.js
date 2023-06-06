f07199cacdddc613c83319d992d53011cfa574e6
Adriane Sutherland <asutherland91@gmail.com>
2023-06-06 11:22:01 -0700

    Add basic app set up

---
+++ src/Components/App/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { CritterGrid } from "../CritterGrid/CritterGrid"
+import "./App.css";
+
+const App = () => {
+
+  return (
+    <CritterGrid />
+  );
+};
+
+export default App;
