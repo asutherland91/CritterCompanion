f8ceb11606da649795a20bbd35bad393126ab1ff
Adriane Sutherland <asutherland91@gmail.com>
2023-06-06 16:58:30 -0700

    Add working got it button

--- src/Components/Icon/Icon.js
+++ src/Components/Icon/Icon.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Icon.css";
 import PropTypes from 'prop-types'
 
 export const Icon = ({ id, image, name }) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
   return(
-    <div id={id} className="icon" >
+    <div id={id} className={`icon ${isClicked ? "fade" : ""}`}>
+<h2 className="name">{name}</h2>
       <div className="image-wrapper">
         <img className="image" src={image} alt={name}/>
       </div>
-      <h2 className="name">{name}</h2>
+      <button className="collected" onClick={handleClick}>
+        Got it!
+      </button>
     </div>
   );
 }
