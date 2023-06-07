d60fab0746c77f633e9755862d925a50660f1a53
Adriane Sutherland <asutherland91@gmail.com>
2023-06-07 11:29:01 -0700

    Add form css and changed some classNames to help with that

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -4,42 +4,91 @@ import PropTypes from 'prop-types';
 
 export const Form = ({setShowBugs, setShowFish, setShowSeaCreatures, setShowMissing, setSort}) => {
 
-  return(
-    <form>
-      <label>
-        Show Missing Critters
-        <input type="checkbox" defaultChecked={false} name="missing-critters" onChange={(event) => {
-          setShowMissing(event.target.checked);
-        }} />
-      </label>
-      <label>
-        Show Bugs
-        <input type="checkbox" defaultChecked={true} name="bugs" onChange={(event) => {
-          setShowBugs(event.target.checked);
-        }} />
-      </label>
-      <label>
-        Show Fish
-        <input type="checkbox" defaultChecked={true} name="fish" onChange={(event) => {
-          setShowFish(event.target.checked);
-        }} />
-      </label>
-      <label>
-        Show Sea Creatures
-        <input type="checkbox" defaultChecked={true} name="sea-creatures" onChange={(event) => {
-          setShowSeaCreatures(event.target.checked);
-        }} />
-      </label>
-      <label>
-        Sort by
-        <select name="sort-by" onChange={(event) => {
-          setSort(event.target.value);
-        }} >
-          <option value="Default">Default</option>
-          <option value="Name">Name</option>
-          <option value="Price">Price</option>
-        </select>
-      </label>
+ return (
+    <form className="form">
+      <div className="form-column">
+        <div className="form-checkbox-label">
+          <label className="form-label">
+            Show Missing Critters
+            <input
+              type="checkbox"
+              className="form-checkbox"
+              defaultChecked={false}
+              name="missing-critters"
+              onChange={(event) => {
+                setShowMissing(event.target.checked);
+              }}
+            />
+          </label>
+        </div>
+        <div className="form-checkbox-label">
+          <label className="form-label">
+            Show Bugs
+            <input
+              type="checkbox"
+              className="form-checkbox"
+              defaultChecked={true}
+              name="bugs"
+              onChange={(event) => {
+                setShowBugs(event.target.checked);
+              }}
+            />
+          </label>
+        </div>
+        <div className="form-checkbox-label">
+          <label className="form-label">
+            Show Fish
+            <input
+              type="checkbox"
+              className="form-checkbox"
+              defaultChecked={true}
+              name="fish"
+              onChange={(event) => {
+                setShowFish(event.target.checked);
+              }}
+            />
+          </label>
+        </div>
+        <div className="form-checkbox-label">
+          <label className="form-label">
+            Show Sea Creatures
+            <input
+              type="checkbox"
+              className="form-checkbox"
+              defaultChecked={true}
+              name="sea-creatures"
+              onChange={(event) => {
+                setShowSeaCreatures(event.target.checked);
+              }}
+            />
+          </label>
+        </div>
+      </div>
+      <div className="form-column">
+        <div className="form-select-label">
+          <label className="form-label">
+            Sort by
+            <select
+              name="sort-by"
+              className="form-sort"
+              onChange={(event) => {
+                setSort(event.target.value);
+            }}>
+              <option value="Default">Default</option>
+              <option value="Name">Name</option>
+              <option value="Price">Price</option>
+            </select>
+          </label>
+        </div>
+      </div>
     </form>
-  )
+  );
+};
+
+Form.propTypes = {
+  setShowBugs: PropTypes.func,
+  setShowFish: PropTypes.func,
+  setShowSeaCreatures: PropTypes.func,
+  setShowMissing: PropTypes.func,
+  setSort: PropTypes.func
 };
