43fd34de4271980263060210c913ef0cd8e1ae22
Adriane Sutherland <asutherland91@gmail.com>
2023-06-08 15:00:34 -0700

    Add month formatter function

--- src/APICalls/utilities.js
+++ src/APICalls/utilities.js
@@ -13,8 +13,9 @@ export const cleanCritterDetailsData = (critterData) => {
   if(critterData.id && critterData["file-name"]) {
     return {
       ...critterData,
-      "file-name": formatName(critterData["file-name"])
-    }
+      "file-name": formatName(critterData["file-name"]),
+      "month-available": formatAvailability(critterData.availability["month-array-northern"])
+    };
   }
 }
 
@@ -24,4 +25,30 @@ const formatName = (name) => {
     .split(" ")
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(" ");
+};
+
+const formatAvailability = (monthArray) => {
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  const formattedMonths = monthArray.map((month) => {
+    const monthIndex = month - 1;
+    return monthNames[monthIndex];
+  });
+
+  const formattedString = formattedMonths.join(", ");
+
+  return formattedString;
 };
